a34a0a18d489b731a169c8704f23c51f09ea7e21
blake-farman <blake.farman@gmail.com>
2026-03-26 11:19:57 -0500

    Yet another attempt at lighthouse CI...

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,22 @@
 module.exports = {
-  ci: {
-    collect: {
-      staticDistDir: './',
+    ci: {
+	collect: {
+	    staticDistDir: './output/web',
+	},
+	assert: {
+	    preset: 'lighthouse:no-pwa',
+	    assertions: {
+		// Enforce a minimum accessibility score
+		'categories:accessibility': ['error', { minScore: 0.9 }],
+
+		// Turn off non-accessibility categories
+		'categories:performance': 'off',
+		'categories:seo': 'off',
+		'categories:best-practices': 'off',
+	    },
+	},
+	upload: {
+	    target: 'temporary-public-storage',
+	},
     },
-    upload: {
-      target: 'temporary-public-storage',
-    },
-  },
 };
