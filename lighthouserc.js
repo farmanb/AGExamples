c56c18084e5f3ecd0f272fe41a5e9ae08e61e7e7
blake-farman <blake.farman@gmail.com>
2026-03-26 11:48:44 -0500

    More LHCI tweaks.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,37 +2,16 @@ module.exports = {
     ci: {
 	collect: {
 	    staticDistDir: './output/web',
+	    numberOfRuns: 1,
+	    settings: {
+		onlyCategories: ['accessibility']
+	    },
 	},
 	assert: {
 	    preset: 'lighthouse:no-pwa',
 	    assertions: {
 		// Enforce a minimum accessibility score
 		'categories:accessibility': ['error', { minScore: 0.9 }],
-
-		// Turn off non-accessibility categories
-		'categories:performance': 'off',
-		'categories:seo': 'off',
-		'categories:best-practices': 'off',
-
-		// Silence all the non-accessibility audit noise
-		'bf-cache': 'off',
-		'deprecations': 'off',
-		'font-display': 'off',
-		'font-display-insight': 'off',
-		'image-delivery-insight': 'off',
-		'inspector-issues': 'off',
-		'meta-description': 'off',
-		'unminified-javascript': 'off',
-		'unsized-images': 'off',
-		'unused-css-rules': 'off',
-		'unused-javascript': 'off',
-		'uses-responsive-images': 'off',
-		'render-blocking-resources': 'off',
-		'render-blocking-insight': 'off',
-		'legacy-javascript': 'off',
-		'modern-image-formats': 'off',
-		'uses-long-cache-ttl': 'off',
-		'cache-insight': 'off',
 	    },
 	},
 	upload: {
